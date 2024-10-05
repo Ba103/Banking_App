d9f472beb952a03bb69ede081a09917888f9f5fd
Balaji <balajir9966@gmail.com>
2024-10-05 10:47:23 +0530

    initial commit

--- src/main/java/in/balaji/Application.java
+++ src/main/java/in/balaji/Application.java
@@ -8,6 +8,8 @@ public class Application {
 
 	public static void main(String[] args) {
 	 SpringApplication.run(Application.class, args);
+
+	 //add  the rest Templet to communicate the other application
 	}
 
 }
